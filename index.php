0a44514414e8592aacd3faa6729d792a6a6a213f
Martin <martinsaposnic@gmail.com>
2018-05-11 15:36:30 -0300

    subo video

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<style>
+
+		.videoContainer {
+		  position: absolute;
+		  width: 100%;
+		  height: 100%;
+		  top: 0;
+		  left: 0;
+		  bottom: 0;
+		  right: 0;
+		  display: flex;
+		  flex-direction: column;
+		  justify-content: center;
+		  align-items: center;
+		}
+
+		iframe {
+		  /* optional */
+		  width: 100%;
+		  height: 100%; 
+		}
+	</style>
+</head>
+<body>
+	<div class="videoContainer">
+    <iframe class="videoContainer__video" width="1920" height="1080" src="http://www.youtube.com/embed/H7Lnw-fk__k?modestbranding=1&autoplay=1&controls=1&fs=0&loop=1&rel=0&showinfo=0&disablekb=1?start=762&mute=1" frameborder="0"></iframe>
+</div>
+</body>
+
+</html>
